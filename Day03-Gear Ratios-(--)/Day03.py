45944e5b8578db331f49a77e4653c74eac0abb75
Olli <102247940+olliklee@users.noreply.github.com>
2023-12-03 22:41:27 +0100

    Day 3 finished

---
+++ Day03-Gear Ratios-(--)/Day03.py
@@ -0,0 +1,99 @@
+# # #  Solutions of Advent of Code
+# # #  Oliver Kleemann
+
+from aoc_helpers import run_puzzles
+
+year, day = "2023", "03"
+final = f"Day{day}_input.txt"
+test = f"Day{day}_input_.txt"
+filename = final
+
+
+def prepare_input(file_name):
+    with open(file_name) as f:
+        content = f.read().split("\n")
+
+    return content
+
+
+def is_sign(char):
+    return not (char.isalnum() or char in ('.', ''))
+
+
+def is_valid_pos(x, y):
+    return 0 <= x < size_x and 0 <= y < size_y
+
+
+def get_adjacents(x, y):
+    possible_adj = [(x - 1, y - 1), (x - 1, y), (x - 1, y + 1),
+                    (x, y - 1), (x, y + 1),
+                    (x + 1, y - 1), (x + 1, y), (x + 1, y + 1)]
+    return [(x, y) for x, y in possible_adj if is_valid_pos(x, y)]
+
+
+def get_number_start(x, y):
+    while x > 0 and puzzle[y][x - 1].isdigit():
+        x -= 1
+
+    return x, y
+
+
+def get_int_at(x, y):
+    result = ''
+    while puzzle[y][x].isdigit():
+        result += puzzle[y][x]
+        x += 1
+        if x > size_x - 1:
+            break
+
+    return int(result)
+
+
+def solve_a():
+    start_num_set = set()
+    # Alle 'Zeichen' in der puzzle Matrix finden
+    signs = [(y, x) for y in range(size_y) for x in range(size_x) if is_sign(puzzle[y][x])]
+
+    # Gehe alle gefundenen Zeichen durch und speichere die Startkoordinaten
+    # jeder einzelnen Zahl in einem Set
+    for sign in signs:
+        adj = get_adjacents(*sign)
+        for y, x in adj:
+            if puzzle[y][x].isdigit():
+                start_num_set.add(get_number_start(x, y))
+
+    # Suche die zugehörige Zahl der gefundenen Startkoordinaten
+    # und addiere sie
+    result = 0
+    for cood in start_num_set:
+        result += get_int_at(*cood)
+
+    return result
+
+
+def solve_b():
+    # Alle Asterisks in der Puzzle Matrix finden
+    signs = [(y, x) for y in range(size_y) for x in range(size_x) if puzzle[y][x] == '*']
+    # Gehe alle gefundenen Zeichen durch und speichere die Startkoordinaten
+    # jeder einzelnen Zahl in einem Set
+    result = 0
+    for asterisk in signs:
+        adj = get_adjacents(*asterisk)
+        temp_set = set()
+        for y, x in adj:
+            if puzzle[y][x].isdigit():
+                temp_set.add(get_number_start(x, y))
+        # Ist es ein Gear mit genau zwei Nachbarn?
+        if len(temp_set) == 2:
+            product = 1
+            for cood in temp_set:
+                product *= get_int_at(*cood)
+            result += product
+
+    return result
+
+
+puzzle = prepare_input(filename)
+size_x, size_y = len(puzzle[0]), len(puzzle)
+
+run_puzzles(day, year, solve_a, solve_b)
