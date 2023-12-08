f897acfeed4b12c39ff97011f2748f648424ba10
Olli <okleemann@icloud.com>
2023-12-08 18:38:52 +0100

    aoc helper - minor optical changes

--- aoc_helpers.py
+++ aoc_helpers.py
@@ -6,10 +6,13 @@ def run_puzzles(d: str, y: str, func_a, func_b):
     Day and year as String,
     Two functions for part1 and part2.
     It prints out the results of the two functions and the time it took to solve it."""
-    dotted_line = '-' * 26
-    print(f"{dotted_line}\n"
+
+    edge = '-' * 26
+    dotted = "|" + '-' * 24 + '|'
+
+    print(f"{edge}\n"
           f"|   AoC {y} - Day {d}    |\n"
-          f"{dotted_line}")
+          f"{dotted}")
 
     start = perf_counter()
     print(f"|  Part 1: {func_a():<14.0f}|")
@@ -17,28 +20,11 @@ def run_puzzles(d: str, y: str, func_a, func_b):
     print(f"|  Part 2: {func_b():<14.0f}|")
     stop = perf_counter()
 
-    print(f"{dotted_line}\n"
+    print(f"{edge}\n"
           f"|      Performance       |\n"
-          f"{dotted_line}\n"
+          f"{dotted}\n"
           f"|  Part 1: {(lap - start) * 100:>7.4f} ms    |\n"
           f"|  Part 2: {(stop - lap) * 100:>7.4f} ms    |\n"
-          f"{dotted_line}\n"
-          f"|  Gesamt: {(stop - start) * 100:>7.4f} ms    |\n"
-          f"{dotted_line}\n")
-
-'''
-
--   AoC 2023 - Day 08    -
---------------------------
-- Part 1: 11567   -
-- Part 2: 9858474970153  -
---------------------------
-Performance
---------------------------
--    Part 1:  0.09455 ms - 
--    Part 2:  3.81688 ms -
-----------------------------------------
--     Gesamt: 3.91143 ms -
---------------------------
-
-'''
+          f"{dotted}\n"
+          f"|   Total: {(stop - start) * 100:>7.4f} ms    |\n"
+          f"{edge}")
